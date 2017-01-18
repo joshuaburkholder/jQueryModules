8fe3632782a44524a0ebfa3f63bd86f84ee89f48
Joshua <Joshua>
2017-01-17 21:00:41 -0500

    uploading addingcontent modules

--- 0.07_addingcontent.js
+++ 0.07_addingcontent.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 
 	//content to be added
-	var content = "<p style='color: red'>You can add me anywhere in the DOM using different jQuery methods!</p>";
-	var content2 = "You can add me anywhere in the DOM using different jQuery methods!";
+	var content = "You can add me anywhere!"
+	var content2 = "You can add me, too!"
 
 	//apend
 	$("#append-example").append(content);
 
-	//prepend
-	$("prepend-example").prepend(content);	
+	//html method
+	$("html-example").html(content);
 
-})
+	//text method
+	$("text-example").text(content2);
+
+});
